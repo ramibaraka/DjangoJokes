2e2d1b248ad7d6955f7fd08434d27c3acfbf6f5b
ramibaraka <ramibarakayhc3l@gmail.com>
2018-06-12 22:15:22 +0200

    Added a generic login view

--- jokesapi/urls.py
+++ jokesapi/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
+    url(r'^rest-auth/', include('rest_auth.urls'))
 ]
